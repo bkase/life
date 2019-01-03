c6d85e1d0aaf049b2926a621f816033059b974a1
Brandon Kase <bkase@o1labs.org>
2019-01-02 21:21:35 -0800

    Fixed the darwin sha for bat

--- src/bat/c.nix
+++ src/bat/c.nix
@@ -4,7 +4,7 @@ let
   arch = if (stdenv.system == "x86_64-linux" || stdenv.isDarwin) then "x86_64" else "386";
   sys = if stdenv.isDarwin then "apple-darwin" else "unknown-linux-gnu";
   ext = "tar.gz";
-  sha = if stdenv.isDarwin then "1b6if0l7qjbiakgzgx6yzkdyasxfh0sma5w28475m0xx6vr4cbil" else "1a98zprcyfqjqahyjgb5aahnml3l11hk51pdw8jwnj5j37b21m5i";
+  sha = if stdenv.isDarwin then "1w43jhjpicb355hxhynq9bcwla0hlyaxxplhafh5sjqa9i1qc2h5" else "1a98zprcyfqjqahyjgb5aahnml3l11hk51pdw8jwnj5j37b21m5i";
   src = fetchurl {
     url = "https://github.com/sharkdp/bat/releases/download/v${version}/bat-v${version}-${arch}-${sys}.${ext}";
     sha256 = sha;
