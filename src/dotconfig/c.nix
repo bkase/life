c9893416acd6f10c005c8984fe2e06ecedd8fdd0
Brandon Kase <brandon.kase@gmail.com>
2022-12-25 16:38:26 -0500

    Makes a nix/ folder for nix-direnv

--- src/dotconfig/c.nix
+++ src/dotconfig/c.nix
@@ -14,6 +14,11 @@
     mkdir -p alacritty/
     cat ${./alacritty.yml} > alacritty/alacritty.yml
 
+    echo "generating nix/..."
+    mkdir -p nix/
+    chmod +wrx nix/
+    chown -R bkase nix/
+
     popd > /dev/null
   '';
 }
